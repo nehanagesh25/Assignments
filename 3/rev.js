8c1a13c485b9032cb630b1ce130c6559cd0eb3e0
nehanagesh25 <neha.n@accionlabs.com>
2018-07-25 10:46:40 +0530

    rev

--- 3/rev.js
+++ 3/rev.js
@@ -1,16 +1,19 @@
 window.onload = function(){
 	var button = document.getElementById("btn");
-	var rev,temp,rem=0;
+	
 	button.onclick = function(){
-		var n = document.getElementById("input").value;
-		temp=n;
-		while(n > 0){
-			rev = rev%10;
-			n = parseInt(n/10);
+		
+	    var n = document.getElementById("input").value; 
+	    var rev = 0, rem = 0;
+		while(n > 0)
+		{
+			rev = n%10;
 			rem = rem*10+rev;
+			n=parseInt(n/10);
 	
 		}
 		var output=document.getElementById("demo");
-		output.innerHTML==rem;
+		output.innerHTML=rem;
+		
 	}
 }
